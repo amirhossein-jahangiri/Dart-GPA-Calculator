631e42c27316d462b6da28bdee5283f0aa92f361
AmirHossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-07-17 23:15:31 +0430

    first commit

---
+++ bin/gpa_calculator.dart
@@ -0,0 +1,5 @@
+import 'package:gpa_calculator/gpa_calculator.dart' as gpa_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gpa_calculator.calculate()}!');
+}
---
+++ test/gpa_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:gpa_calculator/gpa_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
